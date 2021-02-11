a637a13e93fb6e66002888a82007d670f01e2379
Todd Cherveny <ToddChervenyjr@gmail.com>
2021-02-11 13:43:27 -0600

    initialcommit

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> videoGameNames = new List<string>
+            { "Red Dead Redemption", "DOOM", "Uncharted", "W.O.W.", "Myst", "GTA San Andreas", };
+
+            foreach (string s in SortByLength(videoGameNames))
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+        private static IEnumerable<string> SortByLength(List<string> videoGameNames)
+        {
+            var sorted = from s in videoGameNames
+                         orderby s.Length descending
+                         select s;
+            return sorted;
+        }
+    }
+}
